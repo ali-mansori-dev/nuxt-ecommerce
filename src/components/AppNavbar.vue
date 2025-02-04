760c7d8fec73497d652f5953e38c8c1bfcdc827d
Ali Mansoori <71168717+ali-mansori-dev@users.noreply.github.com>
2025-02-05 00:01:53 +0330

    fix to changes

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-toolbar-title class="hidden-xs-and-up">ShopFusion</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- mobile menu button -->
+      <span class="hidden-sm-and-up">
+        <v-btn @click.stop="drawer = !drawer"> Menu </v-btn>
+      </span>
+
+      <!-- desktop & tablet menu -->
+      <v-toolbar-items class="hidden-xs">
+        <v-btn stacked>
+          <v-badge color="error" content="0">
+            <v-icon icon="mdi-cart"></v-icon>
+          </v-badge>
+
+          Cart
+        </v-btn>
+
+        <v-btn variant="tonal" stacked>
+          <v-icon icon="mdi-login"></v-icon>
+
+          Login
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      <v-list dense>
+        <v-btn stacked class="w-100">
+          <v-badge color="error" content="0">
+            <v-icon icon="mdi-cart"></v-icon>
+          </v-badge>
+
+          Cart
+        </v-btn>
+
+        <v-btn variant="tonal" stacked class="w-100">
+          <v-icon icon="mdi-login"></v-icon>
+
+          Login
+        </v-btn>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: "Cart", link: "/", icon: "home" },
+        { title: "Login", link: "cart", icon: "cart" },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Component from "@/components/cart/cartComponent.vue";
+</script>
+
+<template>
+  <v-container
+    ><v-row no-gutters>
+      <v-col v-for="n in 9" :key="n" cols="12" sm="4"
+        ><Component /></v-col></v-row
+  ></v-container>
+</template>
